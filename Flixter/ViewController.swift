b5d87cda5c48afb2a604cdff151679baecf67316
Charlotte Hu <chanyueh@uci.edu>
2019-01-22 14:17:05 -0800

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Chanyue Hu on 1/22/19.
+//  Copyright © 2019 Chanyue Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
